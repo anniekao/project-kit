edbf4602fc26188fc4009bc12ee907719b92f88c
Annie <anniekao@posteo.net>
2019-11-05 11:32:51 -0500

    add login and users services

---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:5000';
+
+const login = async (credentials) => {
+  const res = await axios.post(`${baseUrl}/login`, credentials);
+  return res;
+}
+
+export default {
+  login
+}
---
+++ src/services/users.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const baseUrl = "http://localhost:5000"; // TODO:Replace with Heroku backend url
+
+const getAll = async () => {
+  const res = await axios.get(`${baseUrl}/users`);
+  return res.data;
+}
+
+const createUser = async (userObj, token) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const res = await axios.post(`${baseUrl}/users`, userObj, config);
+  return res.data
+}
+
+
+export default {
+  getAll,
+  createUser
+}
