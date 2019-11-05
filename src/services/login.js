19b2f2e2052a5ee967b877be29345d213c3ba4ab
Annie <anniekao@posteo.net>
2019-11-05 13:22:11 -0500

    delete console log

--- src/services/login.js
+++ src/services/login.js
@@ -4,7 +4,6 @@ const baseUrl = 'http://localhost:5000';
 
 const login = async (credentials) => {
   const res = await axios.post(`${baseUrl}/login`, credentials);
-  console.log("=====res========", res);
   return res.data;
 }
 
