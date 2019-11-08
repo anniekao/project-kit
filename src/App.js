1e8bb414ae3f5782389053cb574814c89a370aa1
Ray J <winorlose1001@gmail.com>
2019-11-08 17:44:48 -0500

    add persist redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import store from "./redux/store";
+// import persistor from "./redux/store";
+// import * as storepersist from "./redux/store"
+// import {store, persistor} from "./redux/store"
 
 import LandingPage from "./pages/landing/landing.component";
 import HomePage from "./pages/home/home.component";
@@ -13,11 +17,14 @@ import CalenderPage from "./pages/calender/calender.component"
 import EventDetailsPage from './pages/event-details/event-details.component';
 import EventHistoryPage from "./pages/event-history/event-history.component";
 import EventContactsPage from "./pages/event-contacts/event-contacts.component";
+import configureStore from "./redux/store"
+let { store, persistor } = configureStore()
 
 function App() {
 
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
@@ -33,6 +40,7 @@ function App() {
           <Route exact path="/events/:eventId/contacts" component={EventContactsPage} />
         </Switch>
       </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -6,12 +10,24 @@ import { composeWithDevTools } from "redux-devtools-extension";
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(
-    applyMiddleware(...middleware)
-  )
-);
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(...middleware)
+    )
+  );
+  let persistor = persistStore(store);
+  return { store, persistor }
+}
+
